535d7943de1bb3422b8f95142ef841fcc7deb68b
Bo Chen <bchen@crusoe.ai>
2025-08-13 22:46:27 +0000

    Add a new crate 'iommufd-bindings'
    Signed-off-by: Bo Chen <bchen@crusoe.ai>

---
+++ iommufd-bindings/src/lib.rs
@@ -0,0 +1,9 @@
+// Copyright © 2025 Crusoe Energy Systems LLC
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#[allow(non_camel_case_types, non_upper_case_globals)]
+pub mod iommufd;
+
+pub use iommufd::*;
